be0b8ad9d6786961869edf4782e607cb2b477059
Xavier Damman <xdamman@gmail.com>
2014-06-14 01:14:35 -0700

    fix error log file

--- twitter.js
+++ twitter.js
@@ -1,4 +1,6 @@
 var RECORD_URL = "http://localhost:1212/record?start=-10&duration=35";
+var TWITTER_USERNAME = "goalflash";
+
 var twitter = require('twitter')
   , humanize = require('humanize')
   , request = require('request');
@@ -6,7 +8,7 @@ var twitter = require('twitter')
 var twit = new twitter(require('./config/twitter'));
 
 console.log(humanize.date("Y-m-d H:i:s")+" Connecting to the Twitter Stream for @"+TWITTER_USERNAME);
-twit.stream('user', {track:'goalflash'}, function(stream) {
+twit.stream('user', {track:TWITTER_USERNAME}, function(stream) {
     console.log(humanize.date("Y-m-d H:i:s")+" Connected");
     stream.on('data', function(data) {
       if(!data.text) return;       
