e6238ec6ce3c46fd4ef4a23f69fcf81aa6658987
Xavier Damman <xdamman@gmail.com>
2014-06-14 12:38:37 -0700

    Removed twitter config file and revoked tokens

--- config/twitter.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    consumer_key: 'rE9DCeTD8dAJFNMv1Cw56k3tN',
-    consumer_secret: 'i9SqzWmakZ9aD95Sm5stopTN05NNwwto4vukVbA7bcU31k8hsi',
-    access_token_key: '2567530321-Lxu0E9upvgJaBv2ec5RqdqoF2ND6sETj3PXCtdH',
-    access_token_secret: 'nyQ9rPwHgQ0PMekVHdEM0KCD4cvPtxeMyOvqV3l6guRI1'
-};
-
