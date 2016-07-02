121b951af97b03b9075f3444c56ffac5f0c56e4c
Xavier Damman <xdamman@gmail.com>
2016-07-02 21:03:32 +0200

    auto detection

--- triggers/twitter.js
+++ triggers/twitter.js
@@ -1,6 +1,8 @@
 // var RECORD_URL_QUERY = "?start=-8&duration=20";
 var RECORD_URL_QUERY = "?window=default";
 var TWITTER_USERNAME = "GoalUpdatesLive";
+var TWITTER_QUERY = "goal #GER #ITA";
+var THRESHOLD_TWEETS_PER_SECOND = 10;
 // var TWITTER_USERNAME = "xdamman_test";
 
 var twitter = require('twitter')
@@ -10,69 +12,58 @@ var twitter = require('twitter')
   , settings = require('../settings.'+env+'.json')
   ;
 
-/*
-var mapping = {
-  "USA": "ned1",
-  "POR": "ned2",
-  "BEL": "ned3",
-  "ALG": "ned1"
-}
-*/
-
-var getChannel = function(tweet) {
-  /*
-  for(var i in mapping) {
-    if(tweet.match(new RegExp(i))) return mapping[i];
-  }
-  */
-  return "ned1";
-}
-
 var twit = new twitter(settings.twitter);
 
 var makeMessage = function(tweet) {
-
+  tweet = "https://twitter.com/" + tweet.user.screen_name + "/statuses/" + tweet.id_str;
   tweet += " \n📺HD Video:"; // 14 chars long
-  
   return tweet;
-
 };
 
-//var lastTweet = 'RT #BRA @GoalFlash: Colombia 3-1* Greece (90\') #COL vs #GRE http://t.co/xsiYol5i5F #GoalFlash #WorldCup';
+var previous_tps = 0;
+var tps = 0;
+var tpm = 0;
+var lastRecording = 0;
 
-/* For testing:
-setTimeout(function() {
-  var tweet= "RT @GoalFlash: Italy 0-1* Uruguay (81') #ITAvsURU http://t.co/xsiYol5i5F #GoalFlash #WorldCup";
-  var text = makeMessage(tweet);
-  var url = "http://localhost:"+settings.port+"/record"+RECORD_URL_QUERY+"&channel="+getChannel(text)+"&text="+encodeURIComponent(text);
-  console.log("Text: ", text);
-      var tweet = { text: text }; 
-      if(tweet.text.match(/correction/i)) {
-        twit.updateStatus(tweet.text, function(data) {}); 
-        return;
-      }
-  console.log("Request: ", url);
-  request(url, function(err, res, body) {
-    console.log(humanize.date("Y-m-d H:i:s")+" "+url+": ", body);
-  });
+setInterval(function() {
+  console.log("tpm: ", tpm);
+  tpm = 0;
+}, 60000);
+
+setInterval(function() {
+
+  console.log("Tweet in last interval: " + tps + " (" + Math.round((tps - previous_tps) / previous_tps * 100) + "%)");
+
+  // If we cross the threshold of tweets per second for two consecutives windows (so total of 4s)
+  if(tps + previous_tps > THRESHOLD_TWEETS_PER_SECOND * 2 && tps > tpm / 60) {
+    if(((new Date).getTime() - lastRecording) < 1000 * 60) {
+      console.error("Last recording less than 60s ago, aborting");
+      return;
+    }
+    var text = makeMessage(tweet);
+    console.log("Making tweet", text);
+    var url = "http://localhost:"+settings.port+"/record"+RECORD_URL_QUERY+"&channel="+getChannel(text)+"&text="+encodeURIComponent(text);
+    console.log("Requesting ", url);
+    lastRecording = new Date;
+    request(url, function(err, res, body) {
+      console.log(humanize.date("Y-m-d H:i:s")+" "+url+": ", body);
+    });
+  }
+  previous_tps = tps;
+  tps = 0;
 }, 1000);
-*/
 
-console.log(humanize.date("Y-m-d H:i:s")+" Connecting to the Twitter Stream for @"+TWITTER_USERNAME);
-twit.stream('user', {track:TWITTER_USERNAME}, function(stream) {
+console.log(humanize.date("Y-m-d H:i:s")+" Connecting to the Twitter Stream for '"+TWITTER_QUERY+"'");
+twit.stream('statuses/filter', {track:TWITTER_QUERY}, function(stream) {
     console.log(humanize.date("Y-m-d H:i:s")+" Connected");
     stream.on('data', function(tweet) {
-      console.log(humanize.date("Y-m-d H:i:s")+" tweet.text: ", tweet);
       if(!tweet.text) return;
-      if(tweet.user.screen_name != TWITTER_USERNAME) return;
+      // console.log(tweet);
+      tps++;
+      tpm++;
+      //console.log(humanize.date("Y-m-d H:i:s")+" tweet.text: ", tweet);
+      //if(tweet.user.screen_name != TWITTER_USERNAME) return;
       // If the tweet is just correcting the score, just tweet it without generating a video
-      var text = makeMessage(tweet.text);
-	console.log("Making tweet", text);
-      var url = "http://localhost:"+settings.port+"/record"+RECORD_URL_QUERY+"&channel="+getChannel(text)+"&text="+encodeURIComponent(text);
-console.log("Requesting ", url);
-      request(url, function(err, res, body) {
-        console.log(humanize.date("Y-m-d H:i:s")+" "+url+": ", body);
-      });
     });
 
     stream.on('error', function(error) {
